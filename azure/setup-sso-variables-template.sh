b6b7769c35d59b5f8115075c3d2ea09edd5f0409
Sumit Malik <sumitmalik51@gmail.com>
2022-12-04 21:39:36 +0530

    Update setup-sso-variables-template.sh

--- azure/setup-sso-variables-template.sh
+++ azure/setup-sso-variables-template.sh
@@ -1,5 +1,7 @@
-export CLIENT_ID=change-me        # Your SSO Provider Client ID
-export CLIENT_SECRET=change-me    # Your SSO Provider Client Secret
-export ISSUER_URI=change-me       # Your SSO Provider Issuer URI
-export JWK_SET_URI=change-me      # Your SSO Provider Json Web Token URI
+export CLIENT_ID=ApplicationID       #  Update with you existing application id from environment details page
+export CLIENT_SECRET=Applicationsecret    # Update with you existing application secret from environment details page
+export ISSUER_URI=https://login.microsoftonline.com/TenantID/v2.0        # Make sure to replace TenantID with your tenant ID
+export JWK_SET_URI=https://login.microsoftonline.com/TenantID/discovery/v2.0/keys # Make sure to replace TenantID with your tenant ID
+
+      # Your SSO Provider Json Web Token URI  # Make sure to replace TenantID with your tenant ID
 export SCOPE=openid,profile,email
