08728fffa2e9fc961af674f5480cde29cb0c2a48
Sumit Malik <sumitmalik51@gmail.com>
2022-12-04 19:30:14 +0530

    Update setup-env-variables-template.sh

--- azure/setup-env-variables-template.sh
+++ azure/setup-env-variables-template.sh
@@ -1,8 +1,8 @@
 export SUBSCRIPTION=subscription-id                 # replace it with your subscription-id
-export RESOURCE_GROUP=resource-group-name           # existing resource group or one that will be created in next steps
-export SPRING_APPS_SERVICE=azure-spring-apps-name   # name of the service that will be created in the next steps
-export LOG_ANALYTICS_WORKSPACE=log-analytics-name   # existing workspace or one that will be created in next steps
-export REGION=region-name                           # choose a region with Enterprise tier support
+export RESOURCE_GROUP=Modernize-java-apps-SUFFIX           # existing resource group or one that will be created in next steps
+export SPRING_APPS_SERVICE=azure-spring-apps-SUFFIX   # name of the service that will be created in the next steps
+export LOG_ANALYTICS_WORKSPACE=acme-log-analytic  # existing workspace or one that will be created in next steps
+export REGION=eastus                           # choose a region with Enterprise tier support
 
 export CART_SERVICE_APP="cart-service"
 export IDENTITY_SERVICE_APP="identity-service"
